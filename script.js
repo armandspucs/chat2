40b26e441c35bbf43b7214d215a89e82ca26f6c9
armandspucs <53978384+armandspucs@users.noreply.github.com>
2022-03-18 11:13:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
 
+    let zina = document.querySelector('.manaZina');
+    let zinas = document.querySelector('.chataZinas');
+
+    zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
 }
 
